c6638c0fb2c989301e3e0c3f47aa733672284852
vqboy1 <152992245+vqboy1@users.noreply.github.com>
2023-12-11 14:45:21 +0300

    Update trying_out_pyqt.py
    added style, made file's save, changed exit way

--- trying_out_pyqt.py
+++ trying_out_pyqt.py
@@ -4,27 +4,56 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(533, 384)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(10, 0, 781, 561))
+        self.widget.setStyleSheet("QWidget{\n"
+                                  "background-color: rgb(190, 190, 190);\n"
+                                  "}")
         self.widget.setObjectName("widget")
-        self.btn_save = QtWidgets.QPushButton(self.widget)
-        self.btn_save.setGeometry(QtCore.QRect(610, 40, 151, 71))
-        self.btn_save.setObjectName("btn_save")
-        self.btn_clear = QtWidgets.QPushButton(self.widget)
-        self.btn_clear.setGeometry(QtCore.QRect(610, 130, 151, 71))
-        self.btn_clear.setObjectName("btn_clear")
-        self.btn_close = QtWidgets.QPushButton(self.widget)
-        self.btn_close.setGeometry(QtCore.QRect(610, 230, 151, 71))
-        self.btn_close.setObjectName("btn_close")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setObjectName("gridLayout")
         self.textEdit = QtWidgets.QTextEdit(self.widget)
-        self.textEdit.setGeometry(QtCore.QRect(20, 20, 551, 521))
+        self.textEdit.setStyleSheet("QTextEdit{\n"
+                                    "background-color: black;\n"
+                                    "color: rgb(0, 165, 80);\n"
+                                    "}")
         self.textEdit.setObjectName("textEdit")
+        self.gridLayout.addWidget(self.textEdit, 0, 0, 1, 1)
+        self.frame = QtWidgets.QFrame(self.widget)
+        self.frame.setStyleSheet("QPushButton {\n"
+                                 "background-color: rgb(76, 187, 23);\n"
+                                 "border: 2px solid black;\n"
+                                 "border-radius: 3px;\n"
+                                 "color: black;\n"
+                                 "font: 13pt \"Avenir Next\";\n"
+                                 "}\n"
+                                 "QFrame {\n"
+                                 "background-color: rgb(134, 134, 134);\n"
+                                 "}")
+        self.frame.setObjectName("frame")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.frame)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.btn_save = QtWidgets.QPushButton(self.frame)
+        self.btn_save.setObjectName("btn_save")
+        self.verticalLayout.addWidget(self.btn_save)
+        self.btn_clear = QtWidgets.QPushButton(self.frame)
+        self.btn_clear.setStyleSheet("QFrame {\n"
+                                     "    background-color: gray;    \n"
+                                     "}")
+        self.btn_clear.setObjectName("btn_clear")
+        self.verticalLayout.addWidget(self.btn_clear)
+        self.btn_close = QtWidgets.QPushButton(self.frame)
+        self.btn_close.setObjectName("btn_close")
+        self.verticalLayout.addWidget(self.btn_close)
+        self.gridLayout.addWidget(self.frame, 0, 1, 1, 1)
+        self.horizontalLayout.addWidget(self.widget)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 533, 24))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -47,13 +76,18 @@ class Ui_MainWindow(object):
         self.textEdit.setText("")
 
     def Save__File(self):
-        S__File = QtWidgets.QFileDialog.getSaveFileName(None,'SaveTextFile','/')
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName(None, "Save File", ".", "Text Files (*.txt);;All Files (*)")
+        if filename:
+            with open(filename, 'w') as file:
+                file.write(self.textEdit.toPlainText())
 
     def Close__File(self):
-        sys.exit(app.exec())
+        app.quit()
+
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
